0318d264a77a43f6813e01b35d964b91b3d94792
Shahzod <shakhzodb1@gmail.com>
2020-02-24 11:11:27 +0500

    Initial Commit

---
+++ qqq/ContentView.swift
@@ -0,0 +1,12 @@
+//
+//  ContentView.swift
+//  qqq
+//
+//  Created by Shahzod Buriev on 2/24/20.
+//  Copyright © 2020 Shahzod Buriev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
